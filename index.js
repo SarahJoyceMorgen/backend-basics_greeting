4223f550eccff1e51080d10f06541e0488cbc8ce
Sarah Morgen <sarah.morgen@outlook.de>
2023-12-19 10:44:34 +0100

    Solution with comment

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ import { createServer } from "node:http";
 
 export const server = createServer((request, response) => {
     response.statusCode = 200;
-    response.end("Hello World");
+    response.end("Hello World");        //wird im Brwoser angezeigt
   });
 
 
-  console.log("Hello World!");
+console.log("Hello World!");  //wird nur im Terminal angezeigt
 
   const port = 3000;
-server.listen(port, (esponse) => {
+server.listen(port, () => {
   console.log(`Server running at http://127.0.0.1:${port}/`);
 });
