3f9c69ddac40f437c52f193a5066491556a89765
Sarah Morgen <sarah.morgen@outlook.de>
2023-12-19 10:41:20 +0100

    Solution

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// import { server } from "./server.js";
+import { createServer } from "node:http";
 
-// server.listen(…
+export const server = createServer((request, response) => {
+    response.statusCode = 200;
+    response.end("Hello World");
+  });
+
+
+  console.log("Hello World!");
+
+  const port = 3000;
+server.listen(port, (esponse) => {
+  console.log(`Server running at http://127.0.0.1:${port}/`);
+});
